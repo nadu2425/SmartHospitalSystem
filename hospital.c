857d86fcf6d000cb8977ee72b902fc4799b0c536
nadu2425 <nanduninimsaraabeydeera@gmail.com>
2026-09-19 12:38:15 +0530

    Add hospital data structures

--- hospital.c
+++ hospital.c
@@ -1,1 +1,19 @@
+#include "hospital.h"
+
+char nameOfPatients[MAXPATIENTS][50];
+int ageOfPatients[MAXPATIENTS];
+int urgencyLevels[MAXPATIENTS];
+int specialityIDs[MAXPATIENTS];
+int wardIDs[MAXPATIENTS];
+int admissionDays[MAXPATIENTS];
+int bedNumbers[MAXPATIENTS];
+
+float waitingTime[MAXPATIENTS];
+float finalBill[MAXPATIENTS];
+float discount[MAXPATIENTS];
+
+int count=0;
+int specialtyQueue[NUMSPECIALITIES]={0};
+int bedOccupancy[NUMWARDS][MAXBEDS]={0};
+
 
