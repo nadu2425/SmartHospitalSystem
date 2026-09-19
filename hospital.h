a73c46293aef26b1b005db7f78324f6c0f745c3e
nadu2425 <nanduninimsaraabeydeera@gmail.com>
2026-09-19 12:49:56 +0530

    Add patient Registration function

--- hospital.h
+++ hospital.h
@@ -1,7 +1,9 @@
 #ifndef HOSPITALH
 #define HOSPITALH
-#define PATIENTS 100
+#define MAXPATIENTS 100
 #define NUMSPECIALITIES 4
 #define NUMWARDS 4
 #define MAXBEDS 20
 #endif
+
+void patientRegister(void);
--- patient.c
+++ patient.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include "hospital.h"
 
+void patientRegister(void){
+    if (count >= MAXPATIENTS){
+        printf("Maximum patient limit reached");
+        return;
+    }
+    printf("\n========== PATIENT REGISTRATION ==========\n");
+
+    printf("Enter patient name: ");
+    scanf(" %49[^\n]", nameOfPatients[count]);
+
+    printf("Enter age: ");
+    scanf("%d", &ageOfPatients[count]);
+
+    printf("Patient registered successfully!\n");
+
+    count++;
+}
